b4be998b1492a8fd448638b656c1020e376797ab
Moray Grieve <moray.grieve@me.com>
2015-06-13 21:50:43 +0100

    Addition of files

---
+++ java/src/com/jtech/iaf/CityBikes.java
@@ -0,0 +1,63 @@
+package com.jtech.iaf;
+
+import com.apama.iaf.plugin.AbstractEventTransport;
+import com.apama.iaf.plugin.EventDecoder;
+import com.apama.iaf.plugin.TransportException;
+import com.apama.iaf.plugin.TransportStatus;
+import com.apama.util.TimestampSet;
+
+
+public class CityBikes extends AbstractEventTransport {
+
+	@Override
+	public void addEventDecoder(String arg0, EventDecoder arg1)
+			throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void cleanup() throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getAPIVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TransportStatus getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeEventDecoder(String arg0) throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void sendTransportEvent(Object arg0, TimestampSet arg1)
+			throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void start() throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() throws TransportException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+}
