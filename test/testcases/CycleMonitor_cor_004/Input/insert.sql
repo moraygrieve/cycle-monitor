ad64ae0889ff421b09e903bc860f673f0902ce79
Moray Grieve <moray.grieve@me.com>
2015-06-28 11:52:29 +0100

    Add test

---
+++ test/testcases/CycleMonitor_cor_004/Input/insert.sql
@@ -0,0 +1,1 @@
+INSERT into rate_alert_config VALUES ('London', 1, 0.1, 60);
---
+++ test/testcases/CycleMonitor_cor_004/run.py
@@ -0,0 +1,21 @@
+from pysys.constants import *
+from apama.correlator import CorrelatorHelper
+from com.jtech.basetest import CycleMonitorTest
+
+class PySysTest(CycleMonitorTest):
+	def execute(self):
+		#start the application
+		self.startCorrelator(xclock=True)
+		self.startScenarioPrinter(self.correlator, filedir=self.input)
+		self.startADBCAdapter(self.correlator, insert='insert.sql')
+		self.initialiseApplication(self.correlator)
+		self.correlator.send('updates.evt')
+		
+		#wait for scenario printer
+		self.waitForSignal('jython.out', expr='REMOVED', condition='==1', timeout=5)
+		
+	def validate(self):
+		exprList=[]
+		exprList.append('ADDED:    id=1, ratio=0.6666, message=Change of 0.17 exceeds limit .1')
+		exprList.append('REMOVED:  id=1')
+		self.assertOrderedGrep('jython.out', exprList=exprList)
